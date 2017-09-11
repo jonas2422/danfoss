3879f2f2c8a89fab936b713d9ef62076bc05beed
Jonas <gerulisjonas1@gmail.com>
2017-09-11 19:40:41 +0200

    navigation

--- header.php
+++ header.php
@@ -46,15 +46,17 @@
 		<nav id="site-navigation" class="main-navigation container">
 		<div class="row">
 		<div class="col-md-12">
-			<div class="navtext">
+			
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'danfossproject' ); ?></button>
+<div class="navtext">
 			<?php
 				wp_nav_menu( array(
 					'theme_location' => 'menu-1',
 					'menu_id'        => 'primary-menu',
 				) );
 			?>
-			</div>
+</div>
+			
 		</div>
 		</div>
 		</nav><!-- #site-navigation -->
